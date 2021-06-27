24e87e7535114577fecacca465aaf732e9080957
charyung <charmaineyung@yahoo.com.hk>
2021-06-26 23:32:17 -0400

    Edit cool cursor

--- src/components/cursor.js
+++ src/components/cursor.js
@@ -4,25 +4,26 @@ function moveCursorOuter(e) {
     const x = e.clientX;
     const y = e.clientY;
 
-    document.getElementById("cursorOuter").style.left = `${x}px`;
-    document.getElementById("cursorOuter").style.top = `${y}px`;
+    document.getElementById("cursorOuter").style.left = `${x - 12}px`;
+    document.getElementById("cursorOuter").style.top = `${y - 12}px`;
 }
 
 export default function Cursor() {
-    const [position, setPosition] = useState({x: 0, y: 0});
+    //const [position, setPosition] = useState({x: 0, y: 0});
 
     useEffect(() => {
-        window.addEventListener("mousemove", moveCursorOuter2);
+        window.addEventListener("mousemove", moveCursorOuter);
 
-        return () => window.removeEventListener("mousemove", moveCursorOuter2);
+        return () => window.removeEventListener("mousemove", moveCursorOuter);
     }, []);
 
-    function moveCursorOuter2(e) {
+    /*function moveCursorOuter2(e) {
         setPosition({x: e.clientX, y: e.clientY});
-    }
+    }*/
     
     return (
-        <img id="cursorOuter" src="/cursorOuterDefault.svg" className="absolute" style={{ left: `${position.x - 12}px`, top: `${position.y - 12}px`, transition: "left .1s, top .1s", position: "fixed" }} ></img>
+        /*<img id="cursorOuter" src="/cursorOuterDefault.svg" className="absolute" style={{ left: `${position.x - 12}px`, top: `${position.y - 12}px`, transition: "left .1s, top .1s", position: "fixed", zIndex: "100" }} ></img>*/
+        <img id="cursorOuter" src="/cursorOuterDefault.svg" className="absolute" style={{ transition: "left .1s, top .1s", position: "fixed", zIndex: "100", pointerEvents: "none" }} ></img>
     )
 }
 

f3b02f55504501419b810369ccf84fc20ceef836
charyung <charmaineyung@yahoo.com.hk>
2021-07-05 22:01:54 -0400

    Add the basic of the spinning nav logo

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,13 +1,13 @@
-import styled, { css } from 'styled-components';
+import { useEffect } from 'react';
+import styled from 'styled-components';
 import tw from 'twin.macro';
+import { throttle } from 'lodash';
 import { A } from '../components/elements';
-import Image from 'next/image'
-//import logoColourless from '../../public/logo.svg';
 
 const NavBase = styled.nav`
     ${tw`fixed w-full top-0 left-0 py-4 z-50`}
     border-bottom: 1px solid var(--dark);
-    backdrop-filter: blur(5px);
+    backdrop-filter: blur(0.75rem);
 `;
 
 const NavInner = styled.div`
@@ -23,11 +23,30 @@ const NavInner = styled.div`
     }
 `;
 
+let prevScrollPosition = 0;
+let ticking = false;
+
+const throttled =  throttle(rotateLogo, 500, { trailing: false });
+
+function rotateLogo(e) {
+    const scrollPosition = window.scrollY;
+    const scrollDiff = scrollPosition - prevScrollPosition;
+    const multiplier = scrollDiff === 0 ? 0 : Math.max(-Math.log(Math.abs(scrollDiff)) + 3, 1);
+    document.getElementById("navLogo").style.transform = `rotate(${scrollDiff * multiplier}deg)`;
+    prevScrollPosition = scrollPosition;
+}
+
 export default function Nav() {
+    useEffect(() => {
+        window.addEventListener("scroll", throttled);
+
+        return () => window.removeEventListener("scroll", throttled);
+    }, []);
+
     return (
         <NavBase>
             <NavInner>
-                <img src="/logoColourless.svg" />
+                <img id="navLogo" src="/logoColourless.svg" style={{ transition: "transform .5s" }} />
                 <A href="#about">About</A>
                 <A href="#sponsors">Sponsors</A>
                 <A href="#pricing">Pricing</A>

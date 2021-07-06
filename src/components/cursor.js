1736edd007b9d4cd1262b7efa6a084c7be3f149b
charyung <charmaineyung@yahoo.com.hk>
2021-07-05 22:02:32 -0400

    Make the cursor white on hover

--- src/components/cursor.js
+++ src/components/cursor.js
@@ -4,8 +4,10 @@ function moveCursorOuter(e) {
     const x = e.clientX;
     const y = e.clientY;
 
-    document.getElementById("cursorOuter").style.left = `${x - 12}px`;
-    document.getElementById("cursorOuter").style.top = `${y - 12}px`;
+    window.requestAnimationFrame(() => {
+        document.getElementById("cursorOuter").style.left = `${x - 12}px`;
+        document.getElementById("cursorOuter").style.top = `${y - 12}px`;
+    });
 }
 
 export default function Cursor() {
@@ -23,7 +25,9 @@ export default function Cursor() {
     
     return (
         /*<img id="cursorOuter" src="/cursorOuterDefault.svg" className="absolute" style={{ left: `${position.x - 12}px`, top: `${position.y - 12}px`, transition: "left .1s, top .1s", position: "fixed", zIndex: "100" }} ></img>*/
-        <img id="cursorOuter" src="/cursorOuterDefault.svg" className="absolute" style={{ transition: "left .1s, top .1s", position: "fixed", zIndex: "100", pointerEvents: "none" }} ></img>
+        <>
+            <img id="cursorOuter" src="/cursorOuterDefault.svg" className="absolute" aria-hidden="true" style={{ transition: "left .1s, top .1s", position: "fixed", zIndex: "100", pointerEvents: "none" }} ></img>
+        </>
     )
 }
 

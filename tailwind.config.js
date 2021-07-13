c4eda74fce8bc6d0c4f9c6bb2af5aba556e55330
charyung <charmaineyung@yahoo.com.hk>
2021-07-12 23:13:50 -0400

    2 spaces -> 4 spaces

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,15 @@
 module.exports = {
-  purge: ['./src/**/*.{js,ts,jsx,tsx}'],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {
-      zIndex: {
-        '-10': '-10',
-      }
+    purge: ['./src/**/*.{js,ts,jsx,tsx}'],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+        extend: {
+            zIndex: {
+                '-10': '-10',
+            }
+        },
     },
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
+    variants: {
+        extend: {},
+    },
+    plugins: [],
 }

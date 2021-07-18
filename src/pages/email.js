756759dd7b45a95823fe89eb2873900db839cf1a
charyung <charmaineyung@yahoo.com.hk>
2021-07-18 19:03:13 -0400

    Update socials icons

--- src/pages/email.js
+++ src/pages/email.js
@@ -54,18 +54,13 @@ export default function Email() {
                                 </a>
                             </td>
                             <td style={{paddingRight: "10px"}}>
-                                <a href="https://twitter.com/cusec">
-                                    <img style={{width: "20px"}} src="/email/twitter.png" />
-                                </a>
-                            </td>
-                            <td style={{paddingRight: "10px"}}>
-                                <a href="https://www.linkedin.com/company/cusec/">
-                                    <img style={{width: "20px"}} src="/email/linkedin.png" />
+                                <a href="https://www.instagram.com/cusecofficial/">
+                                    <img style={{width: "20px"}} src="/email/instagram.png" />
                                 </a>
                             </td>
                             <td>
-                                <a href="https://www.youtube.com/channel/UCZ7j8F6h1CxD-uC0od9rHBw">
-                                    <img style={{width: "20px"}} src="/email/youtube.png" />
+                                <a href="https://www.linkedin.com/company/cusec/">
+                                    <img style={{width: "20px"}} src="/email/linkedin.png" />
                                 </a>
                             </td>
                         </tr>

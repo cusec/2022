8b203c8494eb2e00a614df426a5836f06741ea20
charyung <charmaineyung@yahoo.com.hk>
2021-09-05 02:52:58 -0400

    Make faq box animation happen

--- src/sections/faq/faq.js
+++ src/sections/faq/faq.js
@@ -1,15 +1,29 @@
+import React from "react";
+import styled from "styled-components";
+import tw from "twin.macro";
+
 import { A } from "components/elements";
+import FaqBox from "./faqBox";
+
+const Container = styled.section`
+    ${tw`h-screen flex flex-col justify-center`}
+
+    background-color: var(--dark);
+`;
 
 export default function Faq() {
     const INFO_EMAIL = "info@cusec.com";
 
     return (
-        <section id="faq" className="h-screen flex flex-col justify-center">
+        <Container id="faq">
             <h2>Got questions?</h2>
+            <div>
+                <FaqBox question={"question 1"} answer={"question 1 answer question 1 answer question 1 answer question 1 answer question 1 answer question 1 answer question 1 answer question 1 answer question 1 answer question 1 answer question 1 answer question 1 answer question 1 answer question 1 answer question 1 answer question 1 answer question 1 answer question 1 answer question 1 answer question 1 answer question 1 answer question 1 answer question 1 answer question 1 answer question 1 answer question 1 answer question 1 answer question 1 answer question 1 answer question 1 answer "} />
+            </div>
             <p>
                 Can't find what you're looking for? Send us an email to <A href={`mailto:${INFO_EMAIL}`}>{INFO_EMAIL}</A>
             </p>
 
-        </section>
+        </Container>
     );
 }
--- src/sections/faq/faqBox.js
+++ src/sections/faq/faqBox.js
@@ -1,3 +1,75 @@
-export default function FaqBox({ question, answer }) {
+import React, { useState, useEffect, useLayoutEffect, useRef } from "react";
+import PropTypes from "prop-types";
+import styled from "styled-components";
+import tw from "twin.macro";
+import gsap from "gsap";
 
+const Question = styled.span`
+    color: ${props => props.isOpen ? "var(--primary)" : "var(--light)"}
+`;
+
+const Answer = styled.div`
+    ${tw`relative h-0`}
+    color: var(--light);
+
+    .answer-line {
+        ${tw`w-full h-full block absolute`}
+        content: "";
+        border-top: 1px var(--light) solid;
+
+        /*transform: translateY(${props => props.isOpen ? "100%" : "0"});
+
+        transition: transform .5s;*/
+    }
+
+    .answer-text {
+        opacity: 0;
+        /*opacity: ${props => props.isOpen ? "1" : "0"};
+
+        transition: transform .5s, opacity .5s;*/
+    }
+`;
+
+export default function FaqBox({ question, answer, customClassName }) {
+    const [isOpen, setIsOpen] = useState(false);
+    const [openBox, setOpenBox] = useState(null);
+
+    const answerWrapperRef = useRef();
+    const answerLineRef = useRef();
+    const answerTextRef = useRef();
+
+    useEffect(() => {
+        const animation = gsap.timeline({
+            paused: true,
+            defaults: {
+                duration: 0.25
+            }
+        })
+            .to([answerLineRef.current], { y: "100%" })
+            .to([answerTextRef.current], { opacity: 1 }, "-=0.25")
+            .to([answerWrapperRef.current], { height: "auto" }, "-0.25");
+
+        setOpenBox(animation);
+    }, []);
+
+    useLayoutEffect(() => {
+        if (!openBox) return;
+        isOpen ? openBox.play() : openBox.reverse();
+    }, [isOpen]);
+
+    return (
+        <div className={customClassName} onClick={() => setIsOpen(!isOpen)}>
+            <Question isOpen={isOpen}>{question}</Question>
+            <Answer isOpen={isOpen} ref={answerWrapperRef}>
+                <div className="answer-line" ref={answerLineRef}></div>
+                <p className="answer-text" ref={answerTextRef}>{answer}</p>
+            </Answer>
+        </div>
+    );
 }
+
+FaqBox.propTypes = {
+    question: PropTypes.string.isRequired,
+    answer: PropTypes.string.isRequired,
+    customClassName: PropTypes.string
+};

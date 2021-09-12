546eae8801ce919b0cd4308fca2d78dcc40eae20
charyung <charmaineyung@yahoo.com.hk>
2021-09-12 00:16:42 -0400

    Make the drawing animation thing on scroll for the map

--- src/sections/locations.js
+++ src/sections/locations.js
@@ -9,7 +9,7 @@ import { CITIES } from "constants/cities";
 // Todo: move z-index to globals.css
 const Section = styled.section`
     ${tw`h-screen flex flex-col justify-center relative overflow-hidden z-0`}
-    background-color: var(--secondary);
+    background-color: var(--secondary-highlight);
 
     .map {
         ${tw`absolute`}

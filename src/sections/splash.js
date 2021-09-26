aab90f5221d2d41b973c1801a03158ce2a4e961a
charyung <charmaineyung@yahoo.com.hk>
2021-09-25 22:38:54 -0400

    Add blurry background for the text

--- src/sections/splash.js
+++ src/sections/splash.js
@@ -9,25 +9,34 @@ const Title = styled.div`
     ${tw`flex flex-col md:flex-row leading-none gap-4 items-center`}
 `;
 
-const Header = styled.main`
+const Header = styled.header`
     ${tw`h-screen flex flex-col justify-center`}
-    width: 80vw;
+`;
+
+const Content = styled.div`
+    ${tw`p-12 h-screen md:h-auto`}
+    max-width: 100vw;
+    background: rgba(249, 222, 44, 0.25);
+    box-shadow: 0px 0px 8px var(--shadow);
+    backdrop-filter: blur(0.5rem);
 `;
 
 export default function Splash() {
     return (
         <Header>
-            <Title>
-                <img src="/logo.svg" className="h-24"/>
-                <h1>CUSEC</h1>
-            </Title>
-                    
-            <h2>Canadian University Software Engineering Conference</h2>
-            <Subtitle>January | virtual x in person experience</Subtitle>
+            <Content>
+                <Title>
+                    <img src="/logo.svg" className="h-24"/>
+                    <h1>CUSEC</h1>
+                </Title>
+                        
+                <h2>Canadian University Software Engineering Conference</h2>
+                <Subtitle>January | virtual x in person experience</Subtitle>
 
-            <div className="flex justify-end">
-                <Button className="custom-shadow"> sign up!</Button>
-            </div>
+                <div className="flex justify-end">
+                    <Button className="custom-shadow"> sign up!</Button>
+                </div>
+            </Content>
 
             <TitleSVG/>
         </Header>

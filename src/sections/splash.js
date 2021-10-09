1dd65890ee86554c107e8c1d92a5da868b54ea31
charyung <charmaineyung@yahoo.com.hk>
2021-10-09 01:07:48 -0400

    Move splash buttons to the bottom of the screen on mobile

--- src/sections/splash.js
+++ src/sections/splash.js
@@ -14,7 +14,7 @@ const Header = styled.header`
 `;
 
 const Content = styled.div`
-    ${tw`p-12 h-screen sm:h-auto`}
+    ${tw`p-12 h-screen sm:h-auto flex flex-col`}
     max-width: 100vw;
     background: rgba(249, 222, 44, 0.25);
     box-shadow: 0px 0px 8px var(--shadow);
@@ -35,7 +35,7 @@ export default function Splash() {
                 <h2 className="text-lg md:text-xl">Canadian University Software Engineering Conference</h2>
                 <Subtitle>January 14-16 | Virtual and in person experience</Subtitle>
 
-                <div className="flex justify-end items-center gap-4">
+                <div className="flex justify-end items-center gap-4 mt-auto">
                     <A href={SPEAKERS_URL} className="font-bold">Speak at CUSEC</A>
                     <Button className="custom-shadow">Sponsor us</Button>
                 </div>

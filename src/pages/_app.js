44544f0069b6c6a3bb11dc8a509d06efa444df49
charyung <charmaineyung@yahoo.com.hk>
2022-01-13 16:35:25 -0500

    Add FontAwesome

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,9 @@
 import React from "react";
 
+import { config } from "@fortawesome/fontawesome-svg-core";
+import "@fortawesome/fontawesome-svg-core/styles.css";
+config.autoAddCss = false;
+
 import "../styles/globals.css";
 
 // eslint-disable-next-line react/prop-types

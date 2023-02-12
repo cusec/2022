57103b81b34a915fec4d6ac9301df3758a26d8c3
charyung <charmaineyung@yahoo.com.hk>
2023-02-12 02:48:52 -0500

    Add team info and its page

---
+++ src/constants/teamInfo.js
@@ -0,0 +1,100 @@
+export const TEAM_MEMBERS = [
+    {
+        role: "Chair",
+        name: "HaoHao Du",
+        img: "HaohaoDu.jpg"
+    },
+    {
+        role: "Chair",
+        name: "Isham Singh Behl",
+        img: "IshamBehl.jpg"
+    },
+    {
+        role: "Director of Logistics",
+        name: "Turja Chowdhury",
+        img: "TurjaChowdhury.jpg"
+    },
+    {
+        role: "Logistics Team",
+        name: "Nick Duan",
+        img: "NickDuan.jpg"
+    },
+    {
+        role: "Director of Sponsorship ",
+        name: "Kenny Nguyen",
+        img: "KennyNguyen.jpg"
+    },
+    {
+        role: "Sponsorship Team",
+        name: "Denys Popov",
+        img: "DenysPopov.jpg"
+    },
+    {
+        role: "Sponsorship Team",
+        name: "Ish Khattar",
+        img: "IshKhattar.jpg"
+    },
+    {
+        role: "Director of Speakers",
+        name: "David Chen",
+        img: "DavidChen.jpg"
+    },
+    {
+        role: "Speakers Team",
+        name: "Aisorya Bhandari",
+        img: "AisoryaBhandari.jpg"
+    },
+    {
+        role: "Director of Technology",
+        name: "Omar Flores",
+        img: "OmarFlores.jpg"
+    },{
+        role: "Technology Team",
+        name: "Charmaine Yung",
+        img: "CharmaineYung.jpg"
+    },{
+        role: "Director of Events",
+        name: "Emily Jiawei Chen",
+        img: "EmilyChen.jpg"
+    },
+    {
+        role: "Director of Promotions",
+        name: "Ann Trinh",
+        img: "AnnTrinh.jpg"
+    },
+    {
+        role: "Promotions Team",
+        name: "Andrea Abellera",
+        img: "AndreaAbellera.jpg"
+    },
+    {
+        role: "Head Delegate for uOttawa",
+        name: "Ali Aftab Muhammad",
+        img: "AliMuhammad.jpg"
+    },
+    {
+        role: "Head Delegate for UManitoba",
+        name: "Kunal Rajpal",
+        img: "KunalRajpal.jpg"
+    },
+    {
+        role: "Head Delegate for CarletonU",
+        name: "Aaditya Chopra",
+        img: "AadityaChopra.jpg"
+    },
+    {
+        role: "Head Delegate for UCalgary",
+        name: "Ines Rosito",
+        img: "InesRosito.jpg"
+    },
+    {
+        role: "Head Delegate for UWaterloo",
+        name: "Safiya Mufti",
+        img: "SafiyaMufti.jpg"
+    },
+    {
+        role: "Head Delegate for McGill",
+        name: "Mohanna Shahrad",
+        img: "MohannaShahrad.jpg"
+    }
+];
